b3d6d955f68fd81b840e5964abb346b3cf80193b
Zhandin Pavel <pavelzhandin@gmail.com>
2021-04-15 16:51:16 +0300

    Добавьте файлы проекта.

---
+++ MVCProto/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProto
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVCProto/Controllers/MonitoringController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProto.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        // GET: Monitoring
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewIndex()
+        {
+            return View();
+        }
+
+
+        public ActionResult Monitoring()
+        {
+            return View();
+        }
+    }
+}
