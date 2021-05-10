1b50444e11709ec3aae344d0283da7fb2a9492d6
Zhandin Pavel <pavelzhandin@gmail.com>
2021-05-10 11:17:20 +0300

    added chat (not working)

---
+++ MVCProto/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MVCProto.Startup))]
+
+namespace MVCProto
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
